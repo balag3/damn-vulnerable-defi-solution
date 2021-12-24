dad91caff853c1e5155ba608c667dffc5fe53f47
balag3 <b.kocsis@ildsoftware.ch>
2021-12-24 10:54:46 +0100

    adds extra config

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,7 +12,8 @@ module.exports = {
       compilers: [
         { version: "0.8.7" },
         { version: "0.7.6" },
-        { version: "0.6.6" }
+        { version: "0.6.6" },
+        { version: "0.5.16" }
       ]
     },
     dependencyCompiler: {
